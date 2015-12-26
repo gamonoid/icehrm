cdbea02b46c2816ba7c9a76d0ef5062221e40a04
Thilina Hasantha <thilina.hasantha@gmail.com>
2015-12-27 05:09:33 +0530

    Add purchase modules

--- ext/admin/dashboard/index.php
+++ ext/admin/dashboard/index.php
@@ -190,6 +190,25 @@ include APP_BASE_PATH.'modulejslibs.inc.php';
                 </a>
             </div>
         </div>
+        <div class="col-lg-3 col-xs-12">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>
+                        Purchase
+                    </h3>
+                    <p>
+                        Additional Modules
+                    </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-cart"></i>
+                </div>
+                <a href="http://icehrm.com/modules.php" target="_blank" class="small-box-footer">
+                    Documentation <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
 	</div>
 
     <div id="iceannon">
