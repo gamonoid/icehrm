acf281371b2b721aee8eb6299ba23ae79ff336d7
Thilina Hasantha <thilina.hasantha@gmail.com>
2016-04-17 00:20:39 +0530

    Adding travel module

--- core-ext/scripts/icehrm_master_data.sql
+++ core-ext/scripts/icehrm_master_data.sql
@@ -816,6 +816,10 @@ INSERT INTO `Reports` (`name`, `details`, `parameters`, `query`, `paramOrder`, `
    '["department","date_start","date_end"]', 'Class','Employee Information');
 
 
+INSERT INTO `Settings` (`name`, `value`, `description`, `meta`) VALUES
+  ('Travel: Pre-Approve Travel Request', '0', '','["value", {"label":"Value","type":"select","source":[["1","Yes"],["0","No"]]}]');
+
+
 INSERT INTO `Reports` (`name`, `details`, `parameters`, `query`, `paramOrder`, `type`,`report_group`) VALUES
   ('Employee Time Sheet Report', 'This report list all employee time sheets by employee and date range', '[\r\n[ "employee", {"label":"Employee","type":"select2multi","allow-null":true,"null-label":"All Employees","remote-source":["Employee","id","first_name+last_name"]}],\r\n[ "date_start", {"label":"Start Date","type":"date"}],\r\n[ "date_end", {"label":"End Date","type":"date"}],\r\n[ "status", {"label":"Status","allow-null":true,"null-label":"All Status","type":"select","source":[["Approved","Approved"],["Pending","Pending"],["Rejected","Rejected"]]}]\r\n]', 'EmployeeTimeSheetData', '["employee","date_start","date_end","status"]', 'Class','Time Management');
 
