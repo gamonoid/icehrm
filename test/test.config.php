a4a38a1869201e2f978623f874155301896d1782
Gamonoid <team@gamonoid.com>
2016-11-25 14:29:23 +0100

    add ant buid

--- test/test.config.php
+++ test/test.config.php
@@ -11,18 +11,35 @@ define('SIGN_IN_ELEMENT_MAPPING_FIELD_NAME','employee');
 
 define('CLIENT_NAME', 'app');
 
-//Tests running on vagrant
-define('APP_BASE_PATH', '/vagrant/build/app/');
-define('CLIENT_BASE_PATH', APP_BASE_PATH.'test/');
-define('BASE_URL','http://app.app.dev/');
-define('CLIENT_BASE_URL','http://clients.app.dev/dev/');
+if(!defined('MYSQL_ROOT_USER') || !defined('MYSQL_ROOT_PASS')){
+    //Tests running on vagrant
+    define('APP_BASE_PATH', '/vagrant/build/app/');
+    define('CLIENT_BASE_PATH', APP_BASE_PATH.'test/');
+    define('BASE_URL','http://app.app.dev/');
+    define('CLIENT_BASE_URL','http://clients.app.dev/dev/');
+}else{
+    //Tests running on deploy
+    define('APP_BASE_PATH', realpath(dirname(__FILE__).'/../app')."/");
+    define('CLIENT_BASE_PATH', APP_BASE_PATH.'app/');
+    define('BASE_URL','http://apps.gamonoid.com/icehrmcore/');
+    define('CLIENT_BASE_URL','http://apps.gamonoid.com/icehrm/');
 
-define('APP_DB', 'testing');
-define('APP_USERNAME', 'testing');
-define('APP_PASSWORD', 'testing');
+}
 
-define('MYSQL_ROOT_USER', 'root');
-define('MYSQL_ROOT_PASS', 'dev');
+if(!defined('MYSQL_ROOT_USER') || !defined('MYSQL_ROOT_PASS')){
+    define('APP_DB', 'testing');
+    define('APP_USERNAME', 'testing');
+    define('APP_PASSWORD', 'testing');
+}else{
+    define('APP_DB', 'icehrmht');
+    define('APP_USERNAME', MYSQL_ROOT_USER);
+    define('APP_PASSWORD', MYSQL_ROOT_PASS);
+}
+
+if(!defined('MYSQL_ROOT_USER') || !defined('MYSQL_ROOT_PASS')){
+    define('MYSQL_ROOT_USER', 'root');
+    define('MYSQL_ROOT_PASS', 'dev');
+}
 
 define('APP_HOST', 'localhost');
 define('APP_CON_STR', 'mysqli://'.APP_USERNAME.':'.APP_PASSWORD.'@'.APP_HOST.'/'.APP_DB);
