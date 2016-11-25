564ce43c18da2dea877eba2202afc73519f03491
Gamonoid <team@gamonoid.com>
2016-11-25 14:36:21 +0100

    fix travis-ci build path

--- test/test.config.php
+++ test/test.config.php
@@ -13,8 +13,8 @@ define('CLIENT_NAME', 'app');
 
 if(!defined('MYSQL_ROOT_USER') || !defined('MYSQL_ROOT_PASS')){
     //Tests running on vagrant
-    define('APP_BASE_PATH', TEST_BASE_PATH.'../build/app/');
-    define('CLIENT_BASE_PATH', APP_BASE_PATH.'test/');
+    define('APP_BASE_PATH', TEST_BASE_PATH.'../app/');
+    define('CLIENT_BASE_PATH', TEST_BASE_PATH.'../../deployment/clients/test/');
     define('BASE_URL','http://app.app.dev/');
     define('CLIENT_BASE_URL','http://clients.app.dev/dev/');
 }else{
