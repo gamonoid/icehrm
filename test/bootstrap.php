f803f4265d7c230ccc85935a36b63353860dae99
Thilina Hasantha <thilina.hasantha@gmail.com>
2018-05-01 15:30:54 +0200

    Fix db name issue

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -15,9 +15,8 @@ exec($createDBCommand);
 
 //Run create table script
 $scripts = array(
-    APP_BASE_PATH."scripts/icehrmdb.sql",
-    APP_BASE_PATH."scripts/icehrm_master_data.sql",
-    //APP_BASE_PATH."scripts/icehrm_sample_data.sql",
+    APP_BASE_PATH."core/scripts/icehrmdb.sql",
+    APP_BASE_PATH."core/scripts/icehrm_master_data.sql",
 );
 
 foreach ($scripts as $insql) {
