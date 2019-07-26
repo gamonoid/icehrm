c05e815c17f78bce0693b54097714cfae3031363
Thilina Hasantha <thilina.hasantha@gmail.com>
2019-07-26 04:17:25 +0200

    Add settings endpoint

---
+++ core/src/Settings/Rest/SettingsRestEndPoint.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Settings\Rest;
+
+
+use Classes\IceResponse;
+use Classes\RestEndPoint;
+use Classes\SettingsManager;
+use Classes\UIManager;
+
+class SettingsRestEndPoint extends RestEndPoint
+{
+    public function getMobileSettings()
+    {
+        $sm = SettingsManager::getInstance();
+        $data = [
+            'Company: Logo' => UIManager::getInstance()->getCompanyLogoUrl(),
+            'Company: Name' => $sm->getSetting('Company: Name'),
+            'Attendance: Request Attendance Location on Mobile' =>
+                $sm->getSetting('Attendance: Request Attendance Location on Mobile'),
+        ];
+
+        return new IceResponse(IceResponse::SUCCESS, ['data' => $data]);
+    }
+}
