f87139ef4172c01937393b491470b0b510902c70
Thilina Pituwala <thilina.hasantha@instafreight.com>
2020-05-30 16:12:56 +0200

    Fix cypress tests

--- core/robo/RoboFile.php
+++ core/robo/RoboFile.php
@@ -28,57 +28,57 @@ class RoboFile extends \Robo\Tasks
     }
 
     public function languageList($client) {
-	    $this->includeCientConfig($client);
-	    $this->say("Supported Languages for ". $client);
-	    $language = new \Metadata\Common\Model\SupportedLanguage();
-	    $langs = $language->Find('1 = 1');
+        $this->includeCientConfig($client);
+        $this->say("Supported Languages for ". $client);
+        $language = new \Metadata\Common\Model\SupportedLanguage();
+        $langs = $language->Find('1 = 1');
 
-	    $this->say(print_r(array_column($langs, 'name'), true));
+        $this->say(print_r(array_column($langs, 'name'), true));
     }
 
     public function languageExport($client) {
-	    $this->includeCientConfig($client);
-	    $language = new \Metadata\Common\Model\SupportedLanguage();
-	    $languages = $language->Find('1 = 1 order by id');
-	    $data = [];
-	    $data[0] = [];
-	    $data[0][] = 'Key';
-	    foreach ($languages as $lang) {
-	    	$data[0][] = $lang->name;
-	    	$trans = \Classes\LanguageManager::getTranslations($lang->name);
-	    	$trans = json_decode($trans, true)['messages'][''];
-		    $count = 1;
-	    	foreach ($trans as $enVal => $langVal) {
-			    if (!isset($data[$count])) {
-				    $data[$count] = [];
-				    $data[$count][] = $enVal;
-			    }
-			    $data[$count][] = $langVal[0];
-			    $count += 1;
-		    }
-	    }
-	    $fp = fopen(CLIENT_BASE_PATH.'data/translations_export.csv', 'w');
-	    foreach ($data as $fields) {
-		    fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
-		    fputcsv($fp, $fields);
-	    }
-	    fclose($fp);
-	    $this->say('File saved');
+        $this->includeCientConfig($client);
+        $language = new \Metadata\Common\Model\SupportedLanguage();
+        $languages = $language->Find('1 = 1 order by id');
+        $data = [];
+        $data[0] = [];
+        $data[0][] = 'Key';
+        foreach ($languages as $lang) {
+            $data[0][] = $lang->name;
+            $trans = \Classes\LanguageManager::getTranslations($lang->name);
+            $trans = json_decode($trans, true)['messages'][''];
+            $count = 1;
+            foreach ($trans as $enVal => $langVal) {
+                if (!isset($data[$count])) {
+                    $data[$count] = [];
+                    $data[$count][] = $enVal;
+                }
+                $data[$count][] = $langVal[0];
+                $count += 1;
+            }
+        }
+        $fp = fopen(CLIENT_BASE_PATH.'data/translations_export.csv', 'w');
+        foreach ($data as $fields) {
+            fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+            fputcsv($fp, $fields);
+        }
+        fclose($fp);
+        $this->say('File saved');
     }
 
     public function languageImport($client, $file) {
-		$this->includeCientConfig($client);
+        $this->includeCientConfig($client);
 
-		$language = new \Metadata\Common\Model\SupportedLanguage();
-		$languages = $language->Find('1 = 1 order by id');
-		foreach ($languages as $language) {
-			$str = $this->getUpdatedTranslationString($language->name, $file);
-			file_put_contents(
-				__DIR__.'/../lang/'.$language->name.'.po',
-				$str);
-			$this->say('Updated :'.realpath(__DIR__.'/../lang/'.$language->name.'.po'));
-		}
-	}
+        $language = new \Metadata\Common\Model\SupportedLanguage();
+        $languages = $language->Find('1 = 1 order by id');
+        foreach ($languages as $language) {
+            $str = $this->getUpdatedTranslationString($language->name, $file);
+            file_put_contents(
+                __DIR__.'/../lang/'.$language->name.'.po',
+                $str);
+            $this->say('Updated :'.realpath(__DIR__.'/../lang/'.$language->name.'.po'));
+        }
+    }
 
     public function migrate($client, $action){
         $this->includeCientConfig($client);
@@ -195,45 +195,45 @@ class RoboFile extends \Robo\Tasks
         }
     }
 
-	/**
-	 * @param $lang
-	 * @param $file
-	 * @return mixed
-	 */
-	protected function getUpdatedTranslationString($lang, $file)
-	{
-		$handle = fopen(CLIENT_BASE_PATH . 'data/' . $file, "r");
-		$langColumn = null;
-		/* @var \Gettext\Translations $trans */
-		$trans = \Classes\LanguageManager::getTranslationsObject($lang);
-		while (($data = fgetcsv($handle)) !== FALSE) {
-			if ($langColumn === null) {
-				$currentColumn = 0;
-				foreach ($data as $language) {
-					if ($language === $lang) {
-						$langColumn = $currentColumn;
-						break;
-					}
-					$currentColumn++;
-				}
+    /**
+     * @param $lang
+     * @param $file
+     * @return mixed
+     */
+    protected function getUpdatedTranslationString($lang, $file)
+    {
+        $handle = fopen(CLIENT_BASE_PATH . 'data/' . $file, "r");
+        $langColumn = null;
+        /* @var \Gettext\Translations $trans */
+        $trans = \Classes\LanguageManager::getTranslationsObject($lang);
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            if ($langColumn === null) {
+                $currentColumn = 0;
+                foreach ($data as $language) {
+                    if ($language === $lang) {
+                        $langColumn = $currentColumn;
+                        break;
+                    }
+                    $currentColumn++;
+                }
 
-				if ($langColumn === null) {
-					$this->say('Invalid Language');
-					exit();
-				}
-			} else {
-				/* @var \Gettext\Translation $tran */
-				$tran = $trans->find('', $data[0]);
-				if ($tran !== false) {
-					$tran->setTranslation($data[$langColumn]);
-				} else {
-					$trans->insert('', $data[0]);
+                if ($langColumn === null) {
+                    $this->say('Invalid Language');
+                    exit();
+                }
+            } else {
+                /* @var \Gettext\Translation $tran */
+                $tran = $trans->find('', $data[0]);
+                if ($tran !== false) {
+                    $tran->setTranslation($data[$langColumn]);
+                } else {
+                    $trans->insert('', $data[0]);
                     $tran = $trans->find('', $data[0]);
                     $tran->setTranslation($data[$langColumn]);
-				}
-			}
-		}
+                }
+            }
+        }
 
-		return $trans->toPoString();
-	}
+        return $trans->toPoString();
+    }
 }
