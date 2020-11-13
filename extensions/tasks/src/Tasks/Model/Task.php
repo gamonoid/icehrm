704546fee2c86554bd02c230e252c249e35bc281
Alan Cell <icehrm@web-stalk.com>
2020-11-13 04:54:33 +0100

    Example usage of IceHrm model classes

--- extensions/tasks/src/Tasks/Model/Task.php
+++ extensions/tasks/src/Tasks/Model/Task.php
@@ -8,31 +8,4 @@ use Model\BaseModel;
 class Task extends BaseModel
 {
     public $table = 'Tasks';
-
-    public function getAdminAccess()
-    {
-        return ["get","element","save","delete"];
-    }
-
-    public function getManagerAccess()
-    {
-        return ["get","element"];
-    }
-
-    public function getUserAccess()
-    {
-        return [];
-    }
-
-    public function getAnonymousAccess()
-    {
-        return [];
-    }
-
-    public function getModuleAccess()
-    {
-        return [
-            new ModuleAccess('tasks'),
-        ];
-    }
 }
