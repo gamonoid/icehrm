cb79a551dae297b786cdce9cc8f248d8bc18a98d
ddave <icehrm@web-stalk.com>
2022-05-07 12:41:24 +0200

    Payroll module improvements

--- core/src/Payroll/Common/Model/PayrollColumn.php
+++ core/src/Payroll/Common/Model/PayrollColumn.php
@@ -8,6 +8,7 @@
 
 namespace Payroll\Common\Model;
 
+use Classes\IceResponse;
 use Classes\ModuleAccess;
 use Model\BaseModel;
 
@@ -27,6 +28,96 @@ class PayrollColumn extends BaseModel
     public function getUserAccess()
     {
         return array("get","element");
+    }
+
+    private function reorderColumns()
+    {
+        $payrollColumn = new PayrollColumn();
+        $columns = $payrollColumn->Find('1 = 1 order by colorder', []);
+        $currentOrder = 1;
+        foreach ($columns as $column) {
+            $column->colorder = $currentOrder;
+            $currentOrder++;
+            $column->Save();
+        }
+    }
+
+    private function shouldReorderColumns()
+    {
+        $payrollColumn = new PayrollColumn();
+        $columns = $payrollColumn->Find('colorder <= ?', [0]);
+
+        return count($columns) > 0;
+    }
+
+    private function handleColumnOrderChange($newCol)
+    {
+        $payrollColumn = new PayrollColumn();
+        $columns = $payrollColumn->Find('1 = 1 order by colorder', []);
+        $currentOrder = 1;
+        foreach ($columns as $column) {
+            if (!empty($newCol->id) && (int)$column->id ===  (int)$newCol->id) {
+                continue;
+            }
+            if ((int)$newCol->colorder === (int)$currentOrder) {
+                $currentOrder++;
+            }
+            $column->colorder = $currentOrder;
+            $currentOrder++;
+            $column->Save();
+        }
+    }
+
+    private function decodeCalculationFunction($obj)
+    {
+        if (!empty($obj->calculation_function)) {
+            $obj->calculation_function = base64_decode($obj->calculation_function);
+        }
+
+        return $obj;
+    }
+
+    /**
+     * @param $obj
+     * @return IceResponse
+     */
+    public function executePreSaveActions($obj)
+    {
+//        if ($this->shouldReorderColumns()) {
+//            $this->reorderColumns();
+//        }
+
+        $this->handleColumnOrderChange($obj);
+
+        $obj = $this->decodeCalculationFunction($obj);
+        return new IceResponse(IceResponse::SUCCESS, $obj);
+    }
+
+    /**
+     * @param $obj
+     * @return IceResponse
+     */
+    public function executePreUpdateActions($obj)
+    {
+        $oldColumn = new PayrollColumn();
+        $oldColumn->Load('id = ?', [$obj->id]);
+
+//        if ($this->shouldReorderColumns()) {
+//            $this->reorderColumns();
+//        }
+
+        if (!empty($oldColumn->id) && (int)$oldColumn->coloder !== (int)$obj->colorder) {
+            $this->handleColumnOrderChange($obj);
+        }
+
+        $obj = $this->decodeCalculationFunction($obj);
+
+        return new IceResponse(IceResponse::SUCCESS, $obj);
+    }
+
+    public function executePostDeleteActions($obj)
+    {
+        $this->reorderColumns();
     }
 
     public function getModuleAccess()
