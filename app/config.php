8ceefcd4d2e057bf45cdeda3206172da7ce9aca3
ddave <icehrm@web-stalk.com>
2023-02-13 19:32:48 +0100

    Update config for vagrant dev environment.

--- app/config.php
+++ app/config.php
@@ -1,6 +1,7 @@
 <?php
-$configFile = __DIR__.'/'.getenv( 'ICE_CONFIG_FILE' );
-if ( $configFile ) {
+$file = getenv( 'ICE_CONFIG_FILE' );
+$configFile = __DIR__.'/'.$file;
+if ( $file ) {
     require_once $configFile;
 } else {
     require_once 'config-dev.php';
