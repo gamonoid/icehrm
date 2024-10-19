26e1aa0cde428e3a5b112e1098f218df0162a8af
DDave <team@icehrm.com>
2024-10-19 17:11:56 +0200

    Stop updater if the version is already up to date.

--- updater/updater.php
+++ updater/updater.php
@@ -10,31 +10,36 @@ include(APP_BASE_PATH.'../updater/Updater/UpdateException.php');
 
 try {
 	$downloader = new Downloader(CLIENT_BASE_PATH . 'data/upgrades/',APP_BASE_PATH . '../');
-	$downloadUrl = $downloader->getLatestDownloadUrl();
-	echo 'Download url: ' . $downloadUrl . '<br/>';
 	$version = $downloader->getLatestVersion();
-	echo 'Downloadable version: ' . $version . '<br/>';
-	//echo 'Current version:' . VERSION_NUMBER . '<br/>';
+	echo 'Downloadable version: ' . $version . "\r\n";
+	echo 'Current version:' . VERSION_NUMBER . "\r\n";
 
-	echo 'Downloading release ....'.'<br/>';
+	if ((int)VERSION_NUMBER >= (int)$version) {
+		echo 'Stopping update process, you already have the latest version: ' . $version . "\r\n";
+	}
+
+	$downloadUrl = $downloader->getLatestDownloadUrl();
+	echo 'Download url: ' . $downloadUrl . "\r\n";
+
+	echo 'Downloading release ....'."\r\n";
 	$filePath = $downloader->download();
-	echo 'Download complete:'.$filePath.'<br/>';
+	echo 'Download complete:'.$filePath."\r\n";
 
 	$unzipResult = $downloader->unzip();
 	if ($unzipResult) {
-		echo 'Archive extracted.'.'<br/>';
+		echo 'Archive extracted.'."\r\n";
 	} else {
-		echo 'Archive already extracted.'.'<br/>';
+		echo 'Archive already extracted.'."\r\n";
 	}
 
 	$backupPath = $downloader->backupExisting();
-	echo 'Backup stored at'.$backupPath.'<br/>';
+	echo 'Backup stored at'.$backupPath."\r\n";
 
 	$downloader->replaceCurrentRelease();
-	echo 'New files copied.'.'<br/>';
+	echo 'New files copied.'."\r\n";
 
 } catch (\Updater\UpdateException $e) {
-	echo $e->getMessage().'<br/>';
+	echo $e->getMessage()."\r\n";
 }
 
 
