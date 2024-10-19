93a37667fa0c772870c12dd4a21b490abab3a1aa
DDave <team@icehrm.com>
2024-10-19 05:45:37 +0200

    Updater main file.

---
+++ app/updater.php
@@ -0,0 +1,39 @@
+<?php
+
+use Updater\Downloader;
+
+include ('config.php');
+include(APP_BASE_PATH.'config.base.php');
+include(APP_BASE_PATH.'include.common.php');
+
+try {
+	$downloader = new Downloader(CLIENT_BASE_PATH . 'data/upgrades/',APP_BASE_PATH . '../new/');
+	$downloadUrl = $downloader->getLatestDownloadUrl();
+	echo 'Download url: ' . $downloadUrl . '<br/>';
+	$version = $downloader->getLatestVersion();
+	echo 'Downloadable version: ' . $version . '<br/>';
+	echo 'Current version:' . VERSION_NUMBER . '<br/>';
+
+	echo 'Downloading release ....'.'<br/>';
+	$filePath = $downloader->download();
+	echo 'Download complete:'.$filePath.'<br/>';
+
+	$unzipResult = $downloader->unzip();
+	if ($unzipResult) {
+		echo 'Archive extracted.'.'<br/>';
+	} else {
+		echo 'Archive already extracted.'.'<br/>';
+	}
+
+	$backupPath = $downloader->backupExisting();
+	echo 'Backup stored at'.$backupPath.'<br/>';
+
+	$downloader->replaceCurrentRelease();
+	echo 'New files copied.'.'<br/>';
+
+} catch (\Updater\UpdateException $e) {
+	echo $e->getMessage().'<br/>';
+}
+
+
+
