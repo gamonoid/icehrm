b88f8c8350117afc03c2c3ea52fc810054369418
DDave <team@icehrm.com>
2026-02-06 16:40:24 +1100

    Update app dir

--- app/index.php
+++ app/index.php
@@ -4,6 +4,17 @@ if(!file_exists('config.php')){
 	exit();
 }
 include ('config.php');
+
+if (isset($_REQUEST['auth_code'])) {
+	include APP_BASE_PATH.'auth-code.php';
+}
+
+// Load pro main.php if it exists (for pro-only modules)
+$proMainPath = APP_BASE_PATH . '../extensions/leave_and_performance/main.php';
+if (file_exists($proMainPath)) {
+	require_once $proMainPath;
+}
+
 if(!isset($_REQUEST['g']) || !isset($_REQUEST['n'])){
 header("Location:".CLIENT_BASE_URL."login.php");
 exit();
@@ -16,13 +27,29 @@ $groups = array('admin','modules');
 if($group == 'admin' || $group == 'modules'){
 	$name = str_replace("..","",$name);
 	$name = str_replace("/","",$name);
-	include APP_BASE_PATH.'/'.$group.'/'.$name.'/index.php';
+
+	// Check if module should be loaded from pro directory
+	if (class_exists('ProModuleConfig') && ProModuleConfig::isProModule($group, $name)) {
+		$proModulePath = ProModuleConfig::getProModulePath($group, $name);
+		include $proModulePath.'/index.php';
+	} else {
+		include APP_BASE_PATH.'/'.$group.'/'.$name.'/index.php';
+	}
 }else if ($group == 'extension'){
     $name = str_replace("..","",$name);
     $name = str_replace("/","",$name);
     $moduleName = str_replace('|', '/', $name);
     $moduleGroup = 'extensions';
-    $extensionIndex = APP_BASE_PATH.'../extensions/'.$moduleName.'/web/index.php';
+
+    // Parse extension name and type from moduleName (format: "extensionName/type")
+    $parts = explode('/', $moduleName);
+    $extensionName = $parts[0];
+    $extensionType = isset($parts[1]) ? $parts[1] : 'admin';
+
+    // Get actual path considering grouped extensions
+    require_once APP_BASE_PATH.'extensions/path-resolver.php';
+    $extensionPath = resolveExtensionPath($extensionName, APP_BASE_PATH . '../extensions/');
+    $extensionIndex = $extensionPath . $extensionType . '/web/index.php';
     include APP_BASE_PATH.'extensions/wrapper.php';
 }else{
 	exit();
--- app/updater.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (php_sapi_name() != 'cli') {
-	exit();
-}
-include ('config.php');
-include(APP_BASE_PATH.'../updater/updater.php');
